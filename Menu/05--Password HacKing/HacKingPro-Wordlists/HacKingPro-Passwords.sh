b4e1cdabb3186e7bafcec252f768c2de8feebf77
Aภl๏miuภuຮ <51442719+Anlominus@users.noreply.github.com>
2022-06-17 23:52:38 +0300

    Update HacKingPro-Passwords.sh

--- Menu/05--Password HacKing/HacKingPro-Wordlists/HacKingPro-Passwords.sh
+++ Menu/05--Password HacKing/HacKingPro-Wordlists/HacKingPro-Passwords.sh
@@ -1,7 +1,17 @@
 !#/bin/bash
 clear
-mkdir Wordlists
+pwd
+ls
+mkdir "~/HacKingPro"
+mkdir "~/HacKingPro/Menu"
+mkdir "~/HacKingPro/Menu/05--Password HacKing"
+mkdir "~/HacKingPro/Menu/05--Password HacKing/HacKingPro-Wordlists"
+cd "~/HacKingPro/Menu/05--Password HacKing/HacKingPro-Wordlists"
+pwd
+ls
 echo "\n\t\t HacKingPro/Menu/05--Password HacKing/HacKingPro-Wordlists"
 git clone https://github.com/danielmiessler/SecLists
 git clone https://github.com/scipag/password-list
 git clone https://github.com/jeanphorn/wordlist
+pwd
+ls
