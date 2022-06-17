f6b94044cc16fd14c2371b057e0bf36a08533f32
Aภl๏miuภuຮ <51442719+Anlominus@users.noreply.github.com>
2022-06-17 23:47:24 +0300

    Create HacKingPro-Passwords.sh

---
+++ Menu/05--Password HacKing/HacKingPro-Passwords/HacKingPro-Passwords.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+clear
+mkdir Wordlists
+echo "\n\t\t HacKingPro/Menu/05--Password HacKing/HacKingPro-Wordlists"
+git clone https://github.com/danielmiessler/SecLists
+git clone https://github.com/scipag/password-list
+git clone https://github.com/jeanphorn/wordlist
