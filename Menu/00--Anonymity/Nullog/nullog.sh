8a3e2a38ca9a858792ee688901f1dad488617d74
Aภl๏miuภuຮ <quadcosmos@gmail.com>
2022-06-23 23:38:49 +0300

    Changes To Nullog

--- Menu/00--Anonymity/Nullog/nullog.sh
+++ Menu/00--Anonymity/Nullog/nullog.sh
@@ -1,42 +1,75 @@
 #!/bin/sh
+HacKingProColors(){
+##############################################################################
+# COLORS AND BACKGROUNDS
+##############################################################################
+Color_Off='\033[0m' # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'  # Black
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+Yellow='\033[0;33m' # Yellow
+Blue='\033[0;34m'   # Blue
+Purple='\033[0;35m' # Purple
+Cyan='\033[0;36m'   # Cyan
+White='\033[0;97m'  # White
+
+# Additional colors
+LGrey='\033[0;37m'   # Ligth Gray
+DGrey='\033[0;90m'   # Dark Gray
+LRed='\033[0;91m'    # Ligth Red
+LGreen='\033[0;92m'  # Ligth Green
+LYellow='\033[0;93m' # Ligth Yellow
+LBlue='\033[0;94m'   # Ligth Blue
+LPurple='\033[0;95m' # Light Purple
+LCyan='\033[0;96m'   # Ligth Cyan
+
+
+# Bold
+BBlack='\033[1;30m'  # Black
+BRed='\033[1;31m'    # Red
+BGreen='\033[1;32m'  # Green
+BYellow='\033[1;33m' # Yellow
+BBlue='\033[1;34m'   # Blue
+BPurple='\033[1;35m' # Purple
+BCyan='\033[1;36m'   # Cyan
+BWhite='\033[1;37m'  # White
+
+# Underline
+UBlack='\033[4;30m'  # Black
+URed='\033[4;31m'    # Red
+UGreen='\033[4;32m'  # Green
+UYellow='\033[4;33m' # Yellow
+UBlue='\033[4;34m'   # Blue
+UPurple='\033[4;35m' # Purple
+UCyan='\033[4;36m'   # Cyan
+UWhite='\033[4;37m'  # White
+
+# Background
+On_Black='\033[40m'  # Black
+On_Red='\033[41m'    # Red
+On_Green='\033[42m'  # Green
+On_Yellow='\033[43m' # Yellow
+On_Blue='\033[44m'   # Blue
+On_Purple='\033[45m' # Purple
+On_Cyan='\033[46m'   # Cyan
+On_White='\033[47m'  # White
+}
+HacKingProColors
 
 banner() {
-    printf '\033[0;31m
-         ████               ████
-        ███                   ███
-       ███                     ███
-      ███                       ███
-     ███                         ███
-    ████                         ████
-    ████                         ████
-    ██████       ███████       ██████
-    █████████████████████████████████
-    █████████████████████████████████
-    ███████████████████████████████ 
-      ████ ███████████████████ ████  
-           ███▀███████████▀███       
-          ████──▀███████▀──████      
-          █████───█████───█████      
-          ███████▄█████▄███████      
-           ███████████████████       
-            █████████████████        
-             ███████████████         
-              █████████████          
-               ███████████           
-              ███──▀▀▀──███          
-              ███─█████─███          
-               ███─███─███           
-                █████████
-    \033[0;31m  _   _       _\033[0;37m _             
-    \033[0;31m | \ | |_   _| \033[0;37m| | ___   __ _ 
-    \033[0;31m |  \| | | | | \033[0;37m| |/ _ \ / _` |
-    \033[0;31m | |\  | |_| | \033[0;37m| | (_) | (_| |
-    \033[0;31m |_| \_|\__,_|_\033[0;37m|_|\___/ \__, |
-    \033[0;31m               \033[0;37m         |___/ 
+    echo "
+    ${BRed}  _   _       _${BWhite} _
+    ${BRed} | \ | |_   _| ${BWhite}| | ___   __ _
+    ${BRed} |  \| | | | | ${BWhite}| |/ _ \ / _' |
+    ${BRed} | |\  | |_| | ${BWhite}| | (_) | (_| |
+    ${BRed} |_| \_|\__,_|_${BWhite}|_|\___/ \__, |
+    ${BRed}               ${BWhite}         |___/
 
-            \033[0;31m[\033[0;37mVersion 2.0\033[0;31m]
+            [ Version 2.0 ]
 
-'
+"
 }
 
 remove_login() {
@@ -73,34 +106,36 @@ logs_f() {
     while read log_f
         do
             cat /dev/null $log_f 2>/dev/null
-            printf "\n\033[0;32m[+] \033[0;37mLog deleted: $log_f"
+            printf "\n Log deleted: $log_f"
     done < .logs
     rm -f .logs
 }
 
 main() {
+    clear
     banner
     sleep 1
-    printf '\n\033[0;34m[*] \033[0;37mClearing system login logs\n'
+    echo "${LYellow}[*] ${BYellow}Clearing system login logs\n"
     remove_login
-    printf '\033[0;32m[+] \033[0;37mLogin logs successfully deleted!\n'
+    echo "${LGreen}[+] ${BGreen}Login logs successfully deleted!\n"
     sleep 1
-    printf '\n\033[0;34m[*] \033[0;37mClearing other logs\n'
+    echo "${LYellow}[*] ${BYellow}Clearing other logs\n"
     other_logs
-    printf '\033[0;32m[+] \033[0;37mOther logs successfully deleted!\n'
+    echo "${LGreen}[+] ${BGreen}Other logs successfully deleted!\n"
     sleep 1
-    printf '\n\033[0;34m[*] \033[0;37mClearing system histories\n'
-    bash_history
-    printf '\n\n\033[0;32m[+] \033[0;37mSystem history deleted successfully!\n'
+#    echo "${LYellow}[*] ${BYellow}Clearing system histories\n"
+#    bash_history
+#    echo "${LGreen}[+] ${BGreen}System history deleted successfully!\n"
     sleep 1
-    printf '\n\033[0;34m[*] \033[0;37mDeleting content inside log files'
+    echo "${LYellow}[*] ${BYellow}Deleting content inside log files"
     logs_f
-    printf '\n\n\033[0;32m[+] \033[0;37mLog files cleared successfully!\n'
-    printf '\n\033[0;34m[*] \033[0;37mClear command history "history -c"\n'
+    echo "${LGreen}[+] ${BGreen}Log files cleared successfully!\n"
+    echo "${LYellow}[*] ${BYellow}Clear command history 'history -c'\n"
 }
 
 if [[ $EUID -ne 0 ]]; then
-   printf "\033[0;34m[*] \033[0;37mYou are not running Nullog as root, for that reason only some logs that you have permission will be deleted. Do you really want to continue? [Press ENTER]" 
+   echo "${BYellow}[*] ${BWhite} You are not running Nullog as root, for that reason only some logs that you have permission will be deleted. "
+   echo "Do you really want to continue? \n ${BYellow}[Press ENTER]"
    read
    main
 fi
